30f9e128dd5afde98b9d63a170cce2f13658a9d4
Robert Epprecht <robert.epprecht@gmail.com>
2025-06-17 14:58:36 +0200

    changed morse duration feedback implementation, small colored circle
    D_menu_page.h currently used for developing feedback positions and testing algorhythms
    
    modified:   D_menu_page.h
    modified:   morse_feedback_ATS-MINI_mod.h

--- ats-mini/D_menu_page.h
+++ ats-mini/D_menu_page.h
@@ -7,7 +7,6 @@
 
 extern int32_t morse_feedback_X;
 extern int32_t morse_feedback_Y;
-extern int32_t morse_feedback_H;
 extern int32_t morse_feedback_R;
 
 extern uint32_t morse_feedback_COLOR_OFF;
@@ -18,6 +17,10 @@ extern uint32_t morse_feedback_COLOR_OVERLONG;
 
 extern void signal_morse_in(uint8_t token);
 
+extern unsigned long morse_TimeUnit;
+
+extern uint8_t morse_sep_algo;
+
 void D_menu_display() {
   #if defined RUNNING_ON_ATS_MINI
     MENU.outln(F("\tESP32-SI4732 Receiver"));
@@ -25,33 +28,75 @@ void D_menu_display() {
     DADA(F("unknown gadget"));
   #endif
 
-  MENU.outln(F("\tmorse feedback: X<nnn>\tY<nnn>\tR<nnn>"));
+  MENU.outln(F("\tmorse feedback:\tX<nnn>\tY<nnn>\tR<nnn>"));
+  MENU.out(F("\t\t\t"));
+  MENU.out(morse_feedback_X);
+  MENU.tab();
+  MENU.out(morse_feedback_Y);
+  MENU.tab();
+  MENU.outln(morse_feedback_R);
   MENU.outln(F("\tmorse feedback: 0\t.\t-\t!\tV"));
-}
+
+  MENU.outln(F("\n\tfilled Circle:\t'C'"));
+  MENU.outln(F("\t'x'\t'y'\t'r'"));
+  MENU.tab();
+  MENU.out(morse_feedback_X);
+  MENU.tab();
+  MENU.out(morse_feedback_Y);
+  MENU.tab();
+  MENU.outln(morse_feedback_R);
+  MENU.ln();
+
+  MENU.out(F("\tmorse Time unit: 'T'\t: "));
+  MENU.outln(morse_TimeUnit);
+  MENU.ln();
+
+  MENU.out(F("\talgorithm '1'=tims '2'=limits '3'=diff '4'=limit diffs\t: "));
+  MENU.outln(morse_sep_algo);
+} // D_menu_display()
+
 
 bool D_menu_reaction(char token) {
   long newValue;
 
   switch (token) {
+  case '1':
+    morse_sep_algo=1;
+    break;
+  case '2':
+    morse_sep_algo=2;
+    break;
+  case '3':
+    morse_sep_algo=3;
+    break;
+  case '4':
+    morse_sep_algo=4;
+    break;
+
   case 'X':	// morse feedback X
+  case 'x':
     morse_feedback_X = (int32_t) MENU.calculate_input(morse_feedback_X);
-    //    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
-    drawText("ON", morse_feedback_X, morse_feedback_Y, 1);
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+    spr.pushSprite(0, 0);
     MENU.outln(morse_feedback_X);
     break;
 
   case 'Y':	// morse feedback Y
+  case 'y':
     morse_feedback_Y = (int32_t) MENU.calculate_input(morse_feedback_Y);
-    //    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
-    drawText("DASH", morse_feedback_X, morse_feedback_Y, 1);
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+    spr.pushSprite(0, 0);
     MENU.outln(morse_feedback_Y);
     break;
 
   case 'R':	// morse feedback R
+  case 'r':
     morse_feedback_R = (int32_t) MENU.calculate_input(morse_feedback_R);
     spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+    spr.pushSprite(0, 0);
     MENU.outln(morse_feedback_R);
     break;
+
 
   case '0':
     signal_morse_in('0');
@@ -72,11 +117,25 @@ bool D_menu_reaction(char token) {
   case 'V':
     signal_morse_in('V');
     break;
-    
+
+  case 'C':
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+    spr.pushSprite(0, 0);
+    delay(200);
+
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_LOONG);
+    spr.pushSprite(0, 0);
+    delay(200);
+    break;
+
+  case 'T':	// change morse time scaling for tests
+    morse_TimeUnit = (unsigned long) MENU.calculate_input(morse_TimeUnit);
+    MENU.outln(morse_TimeUnit);
+    break;
+
   default:
     return 0;		// token not found in this menu
   }
 
-  //  delay(1000);	// TODO: REMOVE ################################################################
-  return 1;		// token found in this menu 
-}
+  return 1;		// token found in this menu
+} // D_menu_reaction()
--- ats-mini/morse_feedback_ATS-MINI_mod.h
+++ ats-mini/morse_feedback_ATS-MINI_mod.h
@@ -1,18 +1,25 @@
 /*
   morse_feedback_ATS-MINI_mod.h
 
+  special ADAPTED morse duration feedback for ATS-MINI ESP32-SI4732 Receiver
   loaded by	#include MORSE_MODIFICATED_FEEDBACK
 */
 
-int32_t morse_feedback_X=140;
-int32_t morse_feedback_Y=5;
-int32_t morse_feedback_R=10;
+#if ! defined RUNNING_ON_ATS_MINI
+  #warning "UNKNOWN GADGET...  Compiling special adapted morse duration feedback for ESP32-SI4732 Receiver"));
+#endif
+
+int32_t morse_feedback_X=100;
+int32_t morse_feedback_Y=25;
+int32_t morse_feedback_R=8;
 
 uint32_t morse_feedback_COLOR_OFF=	TFT_BLACK;
 uint32_t morse_feedback_COLOR_DOT=	TFT_YELLOW;
 uint32_t morse_feedback_COLOR_DASH=	TFT_GREEN;
-uint32_t morse_feedback_COLOR_LOONG=	TFT_ORANGE;
+uint32_t morse_feedback_COLOR_LOONG=	TFT_MAGENTA;
 uint32_t morse_feedback_COLOR_OVERLONG=	TFT_RED;
+
+uint8_t morse_sep_algo=4;	// DEVELOPMENT	TODO: REMOVE
 
 #if defined ESP32 && defined TOKEN_LENGTH_FEEDBACK_TASK	// experimental
 
@@ -29,9 +36,8 @@ void /*IRAM_ATTR*/ signal_morse_in(uint8_t token) {
       morse_input_feedback_handle = NULL;
     }
   case '0':	// switch signal OFF
-    // digitalWrite(HARDWARE.morse_output_pin, LOW);
-    // spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_OFF);
-    drawText("     ", morse_feedback_X, morse_feedback_Y, 1);
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_OFF);
+    spr.pushSprite(0, 0);
     break;
 
   case 'S':	// START stop a running task, START a new one and signal ON
@@ -41,27 +47,24 @@ void /*IRAM_ATTR*/ signal_morse_in(uint8_t token) {
     }
     trigger_token_duration_feedback();		// START again
   case '+':	// just switch the signal on
-  case '.':	// signal ON	// i.e. start a dot
-    // digitalWrite(HARDWARE.morse_output_pin, HIGH);		// feedback: pin is TOUCHED, LED on
-    drawText("ON", morse_feedback_X, morse_feedback_Y, 1);
-    // spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+  case '.':	// signal ON	// i.e. start a DOT
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DOT);
+    spr.pushSprite(0, 0);
     break;
 
-  case '-':	// start a dash
-    // noop on MORSE_OUTPUT_PIN
-    // drawText("DASH", morse_feedback_X, morse_feedback_Y, 1);
-    // spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DASH);
+  case '-':	// start a DASH
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_DASH);
+    spr.pushSprite(0, 0);
     break;
-  case '!':	// start a loong
-    // noop on MORSE_OUTPUT_PIN
-    drawText("LOONG", morse_feedback_X, morse_feedback_Y, 1);
-    // spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_LOONG);
+
+  case '!':	// start a LOONG
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_LOONG);
+    spr.pushSprite(0, 0);
     break;
-  case 'V':	// start a overloong
-    // noop on MORSE_OUTPUT_PIN
-    // drawText("_____", morse_feedback_X, morse_feedback_Y, 1);
-    drawText("OVERLONG", morse_feedback_X, morse_feedback_Y, 1);
-    // spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_OVERLONG);
+
+  case 'V':	// start a OVERLOONG
+    spr.fillCircle(morse_feedback_X, morse_feedback_Y, morse_feedback_R, morse_feedback_COLOR_OVERLONG);
+    spr.pushSprite(0, 0);
     break;
 
   default:
@@ -77,28 +80,75 @@ void /*IRAM_ATTR*/ signal_morse_in(uint8_t token) {
 // #define MORSE_INPUT_DURATION_FEEDBACK_SHOW_STACK_USE	// DEBUGGING ONLY
 void morse_input_duration_feedback(void* dummy) {	// s3 version: CONFIG_IDF_TARGET_ESP32S3
   /*
-    give status feedback while the morse key is pressed: when is DASH complete and LOONG has started?  etc
+    give status feedback WHILE THE MORSE KEY IS PRESSED: show when is DASH complete and LOONG has started?  etc
   */
   // signal_morse_in('S');	'S' means START new task and signal ON	// was called by touch_morse_ISR_v3()
-  if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
-    //vTaskDelay((TickType_t) (dashTim * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// ignore beginning of the DASH
 
-    vTaskDelay((TickType_t) (limit_dash_loong * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
+  // test different algorithms for morse key pressed duration feedback	// TODO: REMOVE	DEVELOPMENT ONLY
+  if(morse_sep_algo==1) { // Tims
+    vTaskDelay((TickType_t) (dotTim * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));
     if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
-      signal_morse_in('!');	// '!' means LOONG has started
+      signal_morse_in('-');
+      vTaskDelay((TickType_t) (dashTim * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
 
-      vTaskDelay((TickType_t) (limit_loong_overlong * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
       if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
-	signal_morse_in('V');	// 'V' means OVERLONG has started
+	signal_morse_in('!');	// '!' means LOONG has started
+	vTaskDelay((TickType_t) (loongTim * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on OVERLONG now
+	if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	  signal_morse_in('V');	// 'V' means OVERLONG has started
+	}
       }
     }
-  }
-  morse_input_feedback_handle = NULL;
+  } else
+    if(morse_sep_algo==2) {	// algorhythm "limits"
+      vTaskDelay((TickType_t) (limit_dot_dash * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));
+      if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	signal_morse_in('-');
+	vTaskDelay((TickType_t) (limit_dash_loong * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
+
+	if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	  signal_morse_in('!');	// '!' means LOONG has started
+	  vTaskDelay((TickType_t) (limit_loong_overlong * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on OVERLONG now
+	  if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	    signal_morse_in('V');	// 'V' means OVERLONG has started
+	  }
+	}
+      }
+    } else if(morse_sep_algo==3) { // algorhythm "diff"
+      vTaskDelay((TickType_t) (dotTim * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));
+      if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {		// looks STILL TOUCHED
+	signal_morse_in('-');
+	vTaskDelay((TickType_t) ((dashTim - dotTim) * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
+
+	if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	  signal_morse_in('!');	// '!' means LOONG has started
+	  vTaskDelay((TickType_t) ((loongTim - dashTim - dotTim) * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on OVERLONG now
+	  if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	    signal_morse_in('V');	// 'V' means OVERLONG has started
+	  }
+	}
+      }
+  } else if(morse_sep_algo==4) { // algorhythm "diff with limits"	>>>>>>>>>>>>>>>> LOOKS LIKE THE BEST, TEST <<<<<<<<<<<<<<<<
+      vTaskDelay((TickType_t) (limit_dot_dash * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));
+      if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	signal_morse_in('-');
+	vTaskDelay((TickType_t) ((limit_dash_loong - limit_dot_dash) * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on LOONG now
+
+	if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	  signal_morse_in('!');	// '!' means LOONG has started
+	  vTaskDelay((TickType_t) ((limit_loong_overlong - limit_dash_loong) * morse_TimeUnit / 1000 / portTICK_PERIOD_MS));	// react on OVERLONG now
+	  if(touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {	// looks STILL TOUCHED
+	    signal_morse_in('V');	// 'V' means OVERLONG has started
+	  }
+	}
+      }
+    }
 
 #if defined MORSE_INPUT_DURATION_FEEDBACK_SHOW_STACK_USE	// DEBUGGING ONLY
-    MENU.out(F("morse_input_duration_feedback free STACK "));
-    MENU.outln(uxTaskGetStackHighWaterMark(NULL));
+  MENU.out(F("morse_input_duration_feedback free STACK "));
+  MENU.outln(uxTaskGetStackHighWaterMark(NULL));
 #endif
+  morse_input_feedback_handle = NULL;
   vTaskDelete(NULL);
 } // morse_input_duration_feedback()	// version: CONFIG_IDF_TARGET_ESP32S3
 
@@ -117,7 +167,7 @@ void IRAM_ATTR touch_morse_ISR_v3() {	// s3 ISR for CONFIG_IDF_TARGET_ESP32S3 to
 
   morse_events_cbuf[morse_events_write_i].time = now;	// save time
 
-  // >>>>>>>>>>>>>>>> TOUCHED? <<<<<<<<<<<<<<<<
+  // >>>>>>>>>>>>>>>> TOUCHED? <<<<<<<<<<<<<<<<<
   if (touchInterruptGetLastStatus(MORSE_TOUCH_INPUT_PIN)) {
     morse_events_cbuf[morse_events_write_i].type = 1 /*touched*/;
     signal_morse_in('S');		// 'S' means START a new token
